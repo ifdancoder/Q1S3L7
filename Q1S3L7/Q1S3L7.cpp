5a9b589585e5814b1135c4b2c49c58f8d41051e4
ifdancoder <ifdancoder@gmail.com>
2021-10-09 20:30:51 +0800

    First commit

--- Q1S3L7/Q1S3L7.cpp
+++ Q1S3L7/Q1S3L7.cpp
@@ -1,9 +1,29 @@
 ﻿#include <iostream>
+#include <vector>
+#include "StudentsGroup.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!";
+    {
+        StudentsGroup sg;
+
+        vector<int> evals1{ 4, 4, 5, 4, 4 };
+        sg.addStudent(evals1, "Daniil", "Ivanov");
+
+        vector<int> evals2{ 4, 5, 5, 5, 4 };
+        sg.addStudent(evals2, "Andre", "Maltuguev", "Alexeevich");
+
+        sg.Save();
+    }
+
+    {
+        StudentsGroup sg;
+        sg.Open();
+        cout << sg.GetAllInfo() << "===\n";
+        cout << "Daniil's average: " << sg.GetAverageScore(*sg.genFN("Daniil", "Ivanov")) << endl << endl;
+        cout << "Daniil's info:\n\n" << sg.GetAllInfo(*sg.genFN("Daniil", "Ivanov"));
+    }
     return 0;
 }
